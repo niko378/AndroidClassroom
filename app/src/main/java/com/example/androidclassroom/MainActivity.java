a1202fd1ac606041f59399dd5968619849925575
niko378 <nico.tide2x@gmail.com>
2019-05-25 23:40:08 -0400

    continue login page

--- app/src/main/java/com/example/androidclassroom/MainActivity.java
+++ app/src/main/java/com/example/androidclassroom/MainActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button confirm = findViewById(R.id.create_account_btn);
+        final String fullName = ((EditText) findViewById(R.id.name_field)).getText().toString();
+        final String email = ((EditText) findViewById(R.id.email_field)).getText().toString();
+        final String password = ((EditText) findViewById(R.id.psw_field)).getText().toString();
+        //final char userType = findViewById(((RadioGroup) findViewById(R.id.type_group)).getCheckedRadioButtonId()).toString().charAt(0);
+
+        final MainActivity activity = this;
+        confirm.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                System.out.println("changing activity");
+
+                Intent intent = new Intent(activity, ChannelSelectView.class);
+                intent.putExtra("name", fullName);
+                intent.putExtra("email", email);
+                intent.putExtra("password", password);
+                //intent.putExtra("type", userType);
+
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
     }
